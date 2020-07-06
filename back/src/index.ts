3baf1db7f2bc9d881a014c313d973bc86d07ff99
Clément Doucy <clement.doucy@epitech.eu>
2020-07-06 17:12:17 +0200

    feat(backend): staticly serv front build directory

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import * as path from "path";
 
 import router from "./router";
 
 const port = 5000;
 const host = "0.0.0.0";
 const logFormat = "[:date[web]] :remote-addr :method :url :status :response-time ms";
+const BUILD_PATH = "/public/build"
 
 const app = express();
 
@@ -27,7 +29,13 @@ app.use(morgan(logFormat, {
     stream: process.stdout
 }));
 
+app.use(express.static(BUILD_PATH));
+
 app.use("/api", router);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(BUILD_PATH, "index.html"));
+});
 
 app.listen(port, host, () => {
     console.log(`Server listening on ${host}:${port}`);
